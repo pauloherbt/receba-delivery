6805c568cac8278b5f015eb75a17ba55cb1e18c0
peaga <pauloherbert@alu.ufc.br>
2024-10-06 12:16:59 -0300

    feat: delete restaurant and child tables

--- src/domain/repositories/restaurant-repository/index.js
+++ src/domain/repositories/restaurant-repository/index.js
@@ -4,4 +4,7 @@ export class RestaurantRepository{
     async createRestaurant(data) {
         return (await qb('restaurant').insert(data,"id"))[0];
     }
+    async delete(restaurant_id){
+        return (await qb('restaurant').delete("id").where({id:restaurant_id}))[0];
+    }
 }
--- src/tests/integration/restaurant.spec.js
+++ src/tests/integration/restaurant.spec.js
@@ -1,6 +1,7 @@
 import qb from "../../db";
 import { RestaurantRepository } from "../../domain/repositories/restaurant-repository";
-import { afterEach, beforeAll, expect } from "@jest/globals";
+import { afterEach, beforeAll, beforeEach, expect, test } from "@jest/globals";
+import { createCategory, createRestaurant } from "./helpers";
 
 describe("restaurant creation", () => {
   beforeAll(async () => {
@@ -11,10 +12,10 @@ describe("restaurant creation", () => {
     // Iniciar uma nova transação antes de cada teste
     await qb("restaurant").delete();
   });
-  
-  afterAll(async ()=>{
+
+  afterAll(async () => {
     await qb.destroy();
-  })
+  });
 
   test("should create a  new Restaurant", async () => {
     const mockedRestaurant = {
@@ -33,12 +34,53 @@ describe("restaurant creation", () => {
       address: "Rua Antunino Cunha - Bela Vista",
       image_url: "https://google.com",
     };
-    const restRepo = new RestaurantRepository();
-    try{
-      await restRepo.createRestaurant(mockedRestaurant)
-    }
-    catch(err){
+    try {
+      await createRestaurant(mockedRestaurant);
+    } catch (err) {
       expect(err).toBeDefined();
     }
+  });
+
+  test("should delete a restaurant with no category associated", async () => {
+    const restaurant = await createRestaurant();
+    const result = await new RestaurantRepository().delete(restaurant.id);
+    expect(result).toHaveProperty("id", restaurant.id);
+    expect(
+      await qb("restaurant").select().where({ id: restaurant.id })
+    ).toEqual([]);
+  });
+
+  test("should delete a restaurant with category associated", async () => {
+    const restaurant = await createRestaurant();
+    const category = await createCategory({
+      name: "Sucos",
+      restaurant_id: restaurant.id,
+    });
+    const result = await new RestaurantRepository().delete(restaurant.id);
+    expect(result).toHaveProperty("id", restaurant.id);
+    expect(
+      await qb("restaurant").select().where({ id: restaurant.id })
+    ).toEqual([]);
+    expect(await qb("category").select().where({ id: category.id })).toEqual(
+      []
+    );
+  });
+
+  test("should delete a restaurant with many category associated", async () => {
+    const restaurant = await createRestaurant();
+    //const category = await ;
+    const cat = await Promise.all([
+      createCategory({ name: "Sucos", restaurant_id: restaurant.id }),
+      createCategory({ name: "Tortas", restaurant_id: restaurant.id }),
+      createCategory({ name: "Pãoes", restaurant_id: restaurant.id }),
+    ]);
+    const idArray = cat.map(({ id }) => id);
+
+    const result = await new RestaurantRepository().delete(restaurant.id);
+    expect(result).toHaveProperty("id", restaurant.id);
+    expect(
+      await qb("restaurant").select().where({ id: restaurant.id })
+    ).toEqual([]);
+    expect(await qb("category").select().whereIn("id", idArray)).toEqual([]);
   });
 });
